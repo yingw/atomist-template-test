f4cf247ce900d3132c4cbdbea3b218edd06f729d
yinguowei <yinguowei@gmail.com>
2016-11-13 23:57:56 +0800

    Initial commit from the atomist bot

---
+++ src/main/java/cn/wilmar/atomist/HomeController.java
@@ -0,0 +1,13 @@
+package cn.wilmar.atomist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(path = "/")
+    public String home() {
+        return "Hello REST Microservice World";
+    }
+}
---
+++ src/main/java/cn/wilmar/atomist/MyServiceConfiguration.java
@@ -0,0 +1,9 @@
+package cn.wilmar.atomist;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyServiceConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
